9079ba803016e6ed80898e3152e6fa222a3b53f7
amateop3-sys <amateop3@miumg.edu.gt>
2026-03-27 17:28:27 -0600

    EJERCICIO 1 - CON CONSTRUCTOR

--- src/basicsOfopp/Carro.java
+++ src/basicsOfopp/Carro.java
@@ -2,9 +2,25 @@ package basicsOfopp;
 
 public class Carro {
 	
+	
+	// campos
 	public String brand= null;
 	public String model= null;
 	public String color= null;
+	
+	
+	// constructor
+	public Carro(String brand, String model, String color) {
+		super();
+		this.brand = brand;
+		this.model = model;
+		this.color = color;
+	}
+	
+	// metodo
+	public void carro() {
+		
+	}
 	
 
 }
