35e380d47b8efe707f479833765b76f2605b2e4d
amateop3-sys <amateop3@miumg.edu.gt>
2026-03-27 01:38:18 -0600

    EJERCICIO 1 - CONVERTIR DE INT A FLOAT

---
+++ src/typeCasting/Ejercicios.java
@@ -0,0 +1,24 @@
+package typeCasting;
+
+public class Ejercicios {
+
+	public static void main(String[] args) {
+		
+		
+		// Convertir de int a float
+		int num= 10;
+		
+		System.out.println("EL NUMERO EN INT ES: "+num);
+		
+		double data = num;
+		
+		System.out.println("EL NUMERO EN FLOAT ES: "+data);
+		
+		// ----------------------------------------------------
+		
+		
+		// Convertir
+
+	}
+
+}
