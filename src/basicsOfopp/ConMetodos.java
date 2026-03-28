8b42d5d1b906390f47b88c0145b41e75c9ce75fd
amateop3-sys <amateop3@miumg.edu.gt>
2026-03-27 18:40:05 -0600

    EJERCICIO 1 - CON METODOS

---
+++ src/basicsOfopp/ConMetodos.java
@@ -0,0 +1,17 @@
+package basicsOfopp;
+
+public class ConMetodos {
+	
+	
+	public String brand= null;
+	public String model= null;
+	public String color= null;
+	
+	public void Setcolor(String newcolor) {
+		
+		this.color= newcolor;
+		
+		
+	}
+
+}
--- src/basicsOfopp/Inicio.java
+++ src/basicsOfopp/Inicio.java
@@ -3,7 +3,15 @@ package basicsOfopp;
 public class Inicio {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		ConMetodos car1= new ConMetodos();
+		ConMetodos car2= new ConMetodos();
+		ConMetodos car3= new ConMetodos();
+		
+		
+		car1.Setcolor("RED");
+		car2.Setcolor("GREEN");
+		car3.Setcolor("Blue");
 
 	}
 
